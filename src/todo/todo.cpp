bb675e2f61905063f2affc0d58cbf087703a9613
Svyatoslav Zadorin <puphem@users.noreply.github.com.com>
2023-06-20 12:24:39 +0700

    fixed relative paths

--- src/todo/todo.cpp
+++ src/todo/todo.cpp
@@ -1,19 +1,7 @@
-#include "../functions/functions.h"
+#include <functions/functions.h>
 
 int main() {
     setlocale (LC_ALL, "rus");
     header (0);
-
- /*   ifstream read; 
-    read.open("id.txt"); 
-
-    if (!read.fail()) { 
-        read >> ID; 
-    }
-    else {
-        ID = 0;
-    }
-    read.close(); 
-*/
     startMenu();
 }
